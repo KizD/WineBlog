ab7aa69f0fd0865e481586915089a30c053ebf43
Kiz Dodds <kdodds@verticalresponse.com>
2012-11-17 15:21:07 -0800

    intitial wineblog2 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wineblog2::Application.config.secret_token = 'ac2506a71c3cbc69cae205c2b79b443b4f274978ff1d87f4b7bcc2a3b7545c2b36de126f8284b84f3cf89dca46294c2e9ba0fd52ade932d29ca6ffa7328e9b9b'
